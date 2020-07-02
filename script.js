4305cde249a25dbf903bc8b8b72e1c40d974b226
Ronaldwp <ronaldregan.waiphyo@gmail.com>
2020-07-02 17:16:11 +0630

    updateV.1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hello')
+const imgs = [...document.querySelectorAll('.d-block')];
+const resize = 'img/sizetest.png';
+
+window.addEventListener('load', checkWidth)
+function checkWidth() {
+	let width = window.screen.availWidth;
+
+	if (width < 992) {
+		imgs.forEach( img=> {
+			img.src = resize;
+		});
+	}
+}
