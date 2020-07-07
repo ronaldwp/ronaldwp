71ed0142dfada34bb404c0d89861beb36cd43763
Ronaldwp <ronaldregan.waiphyo@gmail.com>
2020-07-07 12:11:14 +0630

    updateV.1

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const imgs = [...document.querySelectorAll('.d-block')];
-const resize = 'img/sizetest.png';
+const resize = ['FLBookletMockUp_Big.png', 'SiemenMockUpInside2_Big.png','MSME Logo_Big.png', 'Yoma Logo_Big.png'];
 
 window.addEventListener('load', checkWidth)
 function checkWidth() {
 	let width = window.screen.availWidth;
 
 	if (width < 992) {
-		imgs.forEach( img=> {
-			img.src = resize;
+		imgs.forEach( (img , i)=> {
+			img.src = `img/${resize[i]}`
 		});
 	}
-}
+} 
