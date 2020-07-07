59d9fc7b8ebaf30e0040218666552c3e59b121e3
Ronaldwp <ronaldregan.waiphyo@gmail.com>
2020-07-07 13:31:10 +0630

    updatev.1

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const imgs = [...document.querySelectorAll('.d-block')];
 const resize = ['FLBookletMockUp_Big.png', 'SiemenMockUpInside2_Big.png','MSME Logo_Big.png', 'Yoma Logo_Big.png'];
+const phoneBtn = document.querySelector('.phone');
 
-window.addEventListener('load', checkWidth)
+// carosel image size change depending on the screen
+window.addEventListener('load', checkWidth);
 function checkWidth() {
 	let width = window.screen.availWidth;
-
 	if (width < 992) {
 		imgs.forEach( (img , i)=> {
 			img.src = `img/${resize[i]}`
 		});
-	}
-} 
+	};
+};
+
+
+// hide call me btn if the user is on windows
+let agent = navigator.userAgent;
+if(agent.includes('Windows')){
+	console.log("on Windows")
+	phoneBtn.classList.add('hidden');
+} else {
+	console.log(agent)
+	phoneBtn.classList.remove('hidden');
+}
+
+
